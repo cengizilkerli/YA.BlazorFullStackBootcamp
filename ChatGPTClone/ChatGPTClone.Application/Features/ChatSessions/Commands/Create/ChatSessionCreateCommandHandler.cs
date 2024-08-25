c18c68365eb4744e5b676deac55fb1326497350c
Cengiz İLKERLİ <cengizilkerli@gmail.com>
2024-08-25 16:04:53 +0300

    ChatGPTClone Project --> Added ChatSessionCreateCommand and ChatSessionGetAllQuery and DependencyInjections

---
+++ ChatGPTClone/ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.General;
+using MediatR;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Create;
+
+public class ChatSessionCreateCommandHandler : IRequestHandler<ChatSessionCreateCommand, ResponseDto<Guid>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly ICurrentUserServices _currentUserServices;
+
+    public ChatSessionCreateCommandHandler(IApplicationDbContext context, ICurrentUserServices currentUserServices)
+    {
+        _context = context;
+        _currentUserServices = currentUserServices;
+    }
+
+    public async Task<ResponseDto<Guid>> Handle(ChatSessionCreateCommand request, CancellationToken cancellationToken)
+    {
+        var chatSession = request.ToChatSession(_currentUserServices.UserId);
+
+        _context.ChatSessions.Add(chatSession);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new ResponseDto<Guid>(chatSession.Id, "A new chat session was created successfully.");
+    }
+}
---
+++ ChatGPTClone/ChatGPTClone.Application/Features/ChatSessions/Queries/GetAll/ChatSessionGetAllQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace ChatGPTClone.Application.Features.ChatSessions.Queries.GetAll;
+
+public class ChatSessionGetAllQuery : IRequest<List<ChatSessionGetAllDto>>
+{
+    public ChatSessionGetAllQuery()
+    {
+        
+    }
+}
